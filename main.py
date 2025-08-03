4ee140635480ac8be39208ab3e16594b0ab59b1b
yanglkshinka <yangliukunz6022@gmail.com>
2025-08-02 23:28:11 -0500

    Update main.py
    switch to test data

--- main.py
+++ main.py
@@ -415,9 +415,9 @@ def main():
     st.markdown("Add candidate and comp properties with automatic coordinate enrichment and distance analysis.")
     
     # Configuration - Updated for folder structure
-    bucket_name = 'shinka-realestate-gold' # 'lonestar-realestate-test' # 
+    bucket_name = 'lonestar-realestate-test' # 'shinka-realestate-gold' #
     candidate_file = 'candidate/candidate.json'
-    comp_file = 'comps/comps.json'
+    comp_file = 'comps/comp.json'
     
     # Initialize S3 client
     s3_client = init_s3_client()
